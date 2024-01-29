168e589aa8685340d8973b78ff57a081442fa22a
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-29 18:34:15 +0200

    Airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,7 @@ import json
 
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances"""
+    """Serializes instances to a JSON file and deserializes JSON files"""
 
     __file_path = "file.json"
     __objects = {}
@@ -15,7 +15,8 @@ class FileStorage:
         self.__objects[key] = obj
 
     def save(self):
-        serialized_objects = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        serialized_objects = {
+                key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, "w") as json_file:
             json.dump(serialized_objects, json_file)
 
